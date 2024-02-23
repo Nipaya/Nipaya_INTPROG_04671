877728ac72f611e7212da62c2f774a8bd82496ab
unknown <LL144@ccs.local>
2024-02-24 05:22:54 +0800

    Backup
    Backup

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public ViewResult RsvpForm()
+    {
+        return View();
+    }
+
+    {https}
+}
